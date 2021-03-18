76a21a31a380c2e21f5b944380680c9fe3658969
youat0m <senkaved@gmail.com>
2021-03-18 12:57:45 +0300

    add gui

---
+++ testgui.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import filedialog, scrolledtext, Menu, Spinbox
+from worldmap import *
+from json import *
+class app():
+    worldfile = ''
+    world = 0
+    root = Tk()
+    menu = Menu(root)
+    filemenu = Menu(menu)
+    save = Menu(filemenu)
+    txt = scrolledtext.ScrolledText(root, width=60, height=40)
+    def __init__(self):
+        self.root.geometry("1920x1080")
+        self.root.config(menu = self.menu) 
+        self.txt.grid(column=0, row=0 ,rowspan = 6)
+        self.sbut = Button(self.root, command = self.saveworld, text = 'сохранить изменения')
+        self.sbut.grid(column = 0, row = 7, sticky = NSEW)
+        self.filemenu.add_command(label ="открыть", command = self.openfile)
+        self.save.add_command(label = "json", command = self.createJSON)
+        self.save.add_command(label = "фото", command = self.saveImage)
+        self.save.add_command(label = "мир TeaSurvival", command = self.createArray)
+        self.filemenu.add_cascade(label = "сохранить как" , menu = self.save)
+        self.menu.add_cascade(label = 'файл', menu = self.filemenu)
+        self.root.mainloop()
+    def openfile(self):
+        self.worldfile = filedialog.askopenfilename(filetypes = [('TeaSurvival world files','*.js*'),('JSON files','*.json'),('txt files','*.txt'),('all files','*')])
+        self.world = list(GetWorld(filename = self.worldfile))
+        self.txt.insert(INSERT, str(self.world))
+    def saveworld(self):
+        self.world = json.loads(self.txt.get(1.0, END))
+    def saveImage(self):
+        tile = Label(self.root, text = 'размер пикселя: ')
+        self.sb = Spinbox(self.root, width = 5, from_ = 1,to = 40)
+        isbut = Button(self.root, text = 'сохранить как изображение', command =self.createImage)
+        tile.grid(column = 1, row = 0)
+        self.sb.grid(column = 2, row = 0)
+        isbut.grid(column = 1, row = 0 , columnspan = 2, sticky = SE)
+    def createImage(self):
+        createPhoto(int(self.sb.get()),tiles= tiles, array=self.world)
+    def createArray(self):
+        arFile = open(str(filedialog.asksaveasfilename(filetype=[('TeaSurvival world','*.jsts')])),'w')
+        arFile.write(str(self.world))
+    def createJSON(self):
+        arFile = open(str(filedialog.asksaveasfilename(filetype=[('JSON file','*.json')])),'w')
+        arfile.write(json.dump(self.world))
+
+if __name__ == "__main__":
+    a = app()
---
+++ worldmap.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageDraw ,ImageColor
+import json
+tiles = ['rgb(0,150,0)','rgb(0,0,255)','rgb(255,0,0)','rgb(73,73,73)']
+def GetWorld(filename:str):
+	try:
+		file = open(filename, 'r')
+	except:
+		print("file open err")
+		input()
+	try:
+		array =  json.loads(file.read())["map"]
+	except:
+		print("world load err")
+	return array
+# bulding = ['rgb(0,255,0)','rgb(255,255,0)','rgb(80,80,80)','rgb(0,130,0)','rgb(255,150,0)','rgb(128,0,128)','rgb(100,50,0)','rgb(250,220,100)','rgb(0,0,0)','rgb(100,100,100)','rgb(200,200,200)','rgb(0,130,40)','rgb(0,130,60)','yellow','rgb(250,220,100)','rgb(0,0,0)','rgb(0,0,0)','rgb(250,250,100)','rgb(250,250,100)','rgb(250,250,100)']
+
+def createPhoto(size, tiles , array):
+	SEC_SIZE = size - 1
+	image_size = len(array)
+	img = Image.new('RGB',(image_size * size,image_size * size),'gray')
+	draw = ImageDraw.Draw(img)
+	for i in range(len(array)):
+		for j in range(len(array)):
+			draw.rectangle((size * i,size* j,(size* i) + SEC_SIZE,(size* j) + SEC_SIZE), fill = tiles[array[i][j]])
+			print(size* i, tiles[array[i][j]])
+	img.save("test.png")
+	img.show()
+
+if __name__ == '__main__':
+	createPhoto(size=int(input("SIZE: ")),tiles= tiles, array = GetWorld(filename = str(input("file name: "))))
+
